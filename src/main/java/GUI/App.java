f29872f99a626d59ba4048cd555243273d57613b
Tales Scopinho <36113553+talicopanda@users.noreply.github.com>
2020-10-25 14:36:14 -0300

    Adding button functionality to screen

--- src/main/java/GUI/App.java
+++ src/main/java/GUI/App.java
@@ -1,7 +1,11 @@
 package GUI;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
@@ -17,15 +21,26 @@ public class App extends Application {
         var javaVersion = SystemInfo.javaVersion();
         var javafxVersion = SystemInfo.javafxVersion();
 
+        stage.setTitle("SeeMath");
+
+        StackPane layout = new StackPane();
+
         var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
         label.setFont(new Font("Arial", 24));
-        var scene = new Scene(new StackPane(label), 640, 480);
+
+        var button = new Button("Click");
+        button.setOnAction(e -> label.setText("Have you ever had upsexy for dinner?"));
+
+        layout.getChildren().addAll(label, button);
+        layout.setAlignment(button, Pos.BOTTOM_CENTER);
+
+        var scene = new Scene(layout, 640, 480);
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 
 }
