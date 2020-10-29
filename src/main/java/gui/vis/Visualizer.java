990fa9be01e58b84c9113ddd5ad0fc495499bdbc
jacob-sahl <jacobsahlmueller@gmail.com>
2020-10-28 23:42:10 -0400

    Added drawString and drawInt methods to Visualizer and AdditionVisualizer. Beginning implementing drawExpression within AdditionVisualizer

--- src/main/java/gui/vis/Visualizer.java
+++ src/main/java/gui/vis/Visualizer.java
@@ -1,14 +1,27 @@
 package gui.vis;
 
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
-import logic.equations.AdditionEquation;
-import logic.equations.ExpressionTree;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import logic.equations.expression_tree.ExpressionTree;
 
 public abstract class Visualizer {
 
-    public Visualizer(){}
+    Font drawFont = new Font(75);
+    int nodeSize = 100;
 
-    public abstract Pane constructPane(ExpressionTree tree);
+    public abstract Pane drawExpression(ExpressionTree tree);
 
+    public abstract Pane drawInt(int num);
+
+    public Pane drawString(String str){
+        Text text = new Text();
+        text.setFont(drawFont);
+        text.setText(str);
+        FlowPane pane = new FlowPane();
+        pane.getChildren().add(text);
+        return pane;
+    }
 
 }
