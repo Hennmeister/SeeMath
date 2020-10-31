09ecfa9737a45e5b0c4bbe0d923b4afdd7821b33
jacob-sahl <jacobsahlmueller@gmail.com>
2020-10-31 15:14:44 -0400

    Testing AdditionVisualizer.drawExpression: success for simple ExpressionTree (depth 1)

--- src/main/java/demo/AdditionVisDemo.java
+++ src/main/java/demo/AdditionVisDemo.java
@@ -16,6 +16,8 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import gui.vis.Visualizer;
 import gui.vis.AdditionVisualizer;
+import logic.equations.expression_tree.Expression;
+import logic.equations.expression_tree.ExpressionTree;
 
 public class AdditionVisDemo extends Application {
 
@@ -23,7 +25,7 @@ public class AdditionVisDemo extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("This is a test of the AdditionVisualizer.");
 
         // For testing the drawInt and drawString methods
         FlowPane masterPane = new FlowPane();
@@ -32,7 +34,22 @@ public class AdditionVisDemo extends Application {
         masterPane.getChildren().add(additionVisualizer.drawString("+"));
         masterPane.getChildren().add(additionVisualizer.drawInt(4));
 
-        primaryStage.setScene(new Scene(masterPane, 800, 800));
+        // For testing the drawExpression method
+
+        FlowPane drawExpressionPane = new FlowPane();
+
+        Expression ex1 = new Expression(4);
+        Expression ex2 = new Expression("+");
+        Expression ex3 = new Expression(3);
+
+        ExpressionTree expressionTree1 = new ExpressionTree(ex1);
+        ExpressionTree expressionTree2 = new ExpressionTree(ex3);
+        ExpressionTree expressionTree3 = new ExpressionTree(expressionTree1, ex2, expressionTree2);
+
+        drawExpressionPane.getChildren().add(additionVisualizer.drawExpression(expressionTree3));
+
+        //primaryStage.setScene(new Scene(masterPane, 800, 800));
+        primaryStage.setScene(new Scene(drawExpressionPane, 800, 800));
         primaryStage.show();
 
 
