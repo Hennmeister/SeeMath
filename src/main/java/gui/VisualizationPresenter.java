aa42bff2a9489ea3fb8cdcbea81417aa598ea152
Hennmeister <henning.lindig@rogers.com>
2020-11-15 21:08:03 -0500

    added access to UI thread to presenter

--- src/main/java/gui/VisualizationPresenter.java
+++ src/main/java/gui/VisualizationPresenter.java
@@ -16,8 +16,25 @@ public class VisualizationPresenter implements VisualizationCreator {
         this.stage = stage;
     }
 
+    /**
+     * Update the UI
+     * @param eqn The new equation that whose visualization should be shown
+     */
     @Override
     public void updateVisualization(Equation eqn) {
+        // Runs the UI related logic on the javaFX thread
+        Platform.runLater(() -> {
+            // Sample of changing UI - to be changed
+            Label label = new Label(eqn.toString());
+            label.setFont(new Font("Arial", 24));
+            StackPane layout = new StackPane();
+
+            layout.getChildren().addAll(label);
+
+            Scene scene = new Scene(layout, 640, 480);
+            stage.setScene(scene);
+            stage.show();
+        });
         System.out.println("VisualizationPresenter" + eqn);
 
     }
